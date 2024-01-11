0d61aba84ce461d8f593857b2d115f1d8e603767
Zander Hill <zander@xargs.io>
2024-01-10 21:18:51 -0800

    Add hermit and try using release-it

---
+++ .release-it.js
@@ -0,0 +1,32 @@
+module.exports = {
+  "hooks": {
+    "after:bump": "npx auto-changelog -p"
+  },
+  "plugins": {
+    "@release-it/bumper": {
+      "in": {
+        "file": "deno.jsonc",
+        "type": "application/json",
+        "path": "version"
+      },
+      "out": {
+        "file": "deno.jsonc",
+        "type": "application/json",
+        "path": "version"
+      }
+    },
+  },
+  "git": {
+    "changelog": "npx auto-changelog --stdout --commit-limit false -u --template https://raw.githubusercontent.com/release-it/release-it/main/templates/changelog-compact.hbs"
+  },
+  "npm": {
+    "publish": false
+  },
+  "github": {
+    "release": true,
+    "assets": [
+      "dist/*"
+    ],
+    "web": true
+  }
+}
---
+++ TODO.md
@@ -0,0 +1,17 @@
+# TODO
+
+(misc notes)
+
+## release-it
+
+Can use hooks before publishing
+
+```
+"hooks": {
+    "before:init": ["npm run lint", "npm test"],
+    "after:my-plugin:bump": "./bin/my-script.sh",
+    "after:bump": "npm run build",
+    "after:git:release": "echo After git push, before github release",
+    "after:release": "echo Successfully released ${name} v${version} to ${repo.repository}."
+  }
+```
